b4cfcfb032ebe3de4957b86750b17116ba65677d
armyhaylenko <armyhaylenko@gmail.com>
2024-12-07 00:57:05 +0200

    wip: add p2p code

---
+++ src/filesystem.rs
@@ -0,0 +1,233 @@
+use std::{collections::HashMap, sync::Arc};
+
+use blake3::{Hash, Hasher};
+use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+use tokio::sync::{Mutex, RwLock};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Date not found: {0}")]
+    BadDate(NaiveDate),
+    #[error("Something went wrong...")]
+    InternalError,
+}
+
+const AVAILABLE_DATES: [NaiveDate; 30] = [
+    NaiveDate::from_ymd_opt(2024, 12, 1).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 2).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 3).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 4).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 5).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 6).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 7).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 8).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 9).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 10).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 11).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 12).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 13).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 14).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 15).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 16).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 17).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 18).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 19).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 20).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 21).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 22).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 23).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 24).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 25).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 26).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 27).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 28).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 29).unwrap(),
+    NaiveDate::from_ymd_opt(2024, 12, 30).unwrap(),
+];
+
+const AVAILABLE_TIMES: [NaiveTime; 6] = [
+    NaiveTime::from_hms_opt(0, 0, 0).unwrap(),
+    NaiveTime::from_hms_opt(4, 0, 0).unwrap(),
+    NaiveTime::from_hms_opt(8, 0, 0).unwrap(),
+    NaiveTime::from_hms_opt(12, 0, 0).unwrap(),
+    NaiveTime::from_hms_opt(16, 0, 0).unwrap(),
+    NaiveTime::from_hms_opt(20, 0, 0).unwrap(),
+];
+
+fn gen_date_distribution() -> Vec<NaiveDate> {
+    use rand::seq::SliceRandom;
+    let mut dates: Vec<NaiveDate> = AVAILABLE_DATES
+        .choose_multiple(&mut rand::thread_rng(), 20)
+        .cloned()
+        .collect();
+    dates.sort();
+    dates
+}
+
+fn gen_image_times(date: NaiveDate) -> Vec<NaiveDateTime> {
+    use rand::seq::SliceRandom;
+    let mut times: Vec<NaiveDateTime> = AVAILABLE_TIMES
+        .choose_multiple(&mut rand::thread_rng(), 3)
+        .cloned()
+        .map(|t| NaiveDateTime::new(date, t))
+        .collect();
+    times.sort();
+    times
+}
+
+#[derive(Clone)]
+pub struct Image {
+    /// the image itself, for the purposes of test task is just random bytes
+    pub data: Vec<u8>,
+    /// the actual date & time value we care about for ordering
+    pub created_at: NaiveDateTime,
+}
+
+impl std::fmt::Debug for Image {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "|{}| {:x?}...", self.created_at, &self.data[..5])
+    }
+}
+
+impl Image {
+    fn random(created_at: NaiveDateTime) -> Self {
+        let rand_slice: [u8; 32] = rand::random();
+        Self {
+            created_at,
+            data: rand_slice.to_vec(),
+        }
+    }
+
+    fn hash(&self) -> Hash {
+        #[cfg(debug_assertions)]
+        tracing::trace!(img_name = %self.created_at, "Hashing self");
+
+        Hasher::new().update_rayon(&self.data).finalize()
+    }
+}
+
+#[derive(Debug)]
+pub struct Directory {
+    /// actual value we care about for the sync
+    pub date: NaiveDate,
+    /// images inside of the directory, assuming sorted by creation
+    pub images: Vec<(Hash, Image)>,
+}
+
+/// This struct holds the metadata of the directory.
+/// Primary sync primitive.
+#[derive(Debug, Clone, Copy)]
+pub struct DirectoryMetadata {
+    pub dir_hash: Hash,
+    pub fs_index: usize,
+}
+
+impl Directory {
+    fn hash(&self) -> Hash {
+        let mut hasher = Hasher::new();
+        for (image_hash, _) in self.images.iter() {
+            hasher.update_rayon(image_hash.as_bytes());
+        }
+
+        hasher.finalize()
+    }
+}
+
+#[derive(Debug)]
+pub struct NaiveFs {
+    /// Root hash of the filesystem. Once all the root hashes
+    /// of the peers become equal, we know the sync is finished.
+    root_hash: Arc<RwLock<Hash>>,
+    /// Assuming that we only have directories in the fs
+    /// The obv choice for such task is an MPT, but i want to rely
+    /// on libraries as least as possible.
+    ///
+    /// the RwLock is used for easy concurrent accesses
+    dir_metadatas: Arc<RwLock<HashMap<NaiveDate, DirectoryMetadata>>>,
+    dirs: Arc<Mutex<Vec<Directory>>>,
+}
+
+impl NaiveFs {
+    pub fn random() -> Self {
+        let available_dates = gen_date_distribution();
+        let mut root_hasher = Hasher::new();
+        let gen_directory_images = |date: NaiveDate| -> Vec<(Hash, Image)> {
+            gen_image_times(date)
+                .into_iter()
+                .map(|t| {
+                    let i = Image::random(t);
+                    let hash = i.hash();
+                    (hash, i)
+                })
+                .collect()
+        };
+        let dirs: Vec<(NaiveDate, Directory, DirectoryMetadata)> = available_dates
+            .into_iter()
+            .enumerate()
+            .map(|(idx, d)| {
+                let dir = Directory {
+                    date: d,
+                    images: gen_directory_images(d),
+                };
+                let dir_hash = dir.hash();
+                root_hasher.update_rayon(dir_hash.as_bytes());
+                let dir_metadata = DirectoryMetadata {
+                    dir_hash,
+                    fs_index: idx,
+                };
+                (d, dir, dir_metadata)
+            }) // return both the date and the dir to build
+            // the hashmap
+            .collect();
+
+        let directory_metadatas = dirs
+            .iter()
+            .map(|(date, _, meta)| (*date, *meta))
+            .collect::<Vec<(NaiveDate, DirectoryMetadata)>>();
+        let dirs = dirs.into_iter().map(|(_, dir, _)| dir).collect();
+
+        Self {
+            root_hash: Arc::new(RwLock::new(root_hasher.finalize())),
+            dir_metadatas: Arc::new(RwLock::new(HashMap::from_iter(directory_metadatas))),
+            dirs: Arc::new(Mutex::new(dirs)),
+        }
+    }
+
+    pub async fn dir_state(&self, date: NaiveDate) -> Result<Hash, Error> {
+        Ok(self
+            .dir_metadatas
+            .read()
+            .await
+            .get(&date)
+            .ok_or(Error::BadDate(date))?
+            .dir_hash)
+    }
+
+    pub async fn synced_to_root(&self, root_hash: Hash) -> bool {
+        *self.root_hash.read().await == root_hash
+    }
+
+    pub async fn get_images_by_hashes(
+        &self,
+        date: NaiveDate,
+        hashes: &[Hash],
+    ) -> Result<Vec<Image>, Error> {
+        let dir_metadatas = self.dir_metadatas.read().await;
+        let DirectoryMetadata { fs_index, .. } = dir_metadatas
+            .get(&date)
+            .cloned()
+            .ok_or(Error::BadDate(date))?;
+        std::mem::drop(dir_metadatas);
+        let dirs = self.dirs.lock().await;
+        let dir = dirs.get(fs_index).ok_or(Error::BadDate(date))?;
+        let images = dir
+            .images
+            .iter()
+            .filter_map(|(hash, i)| hashes.iter().find(|h| **h == *hash).map(|_| i))
+            .cloned()
+            .collect();
+
+        Ok(images)
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,155 @@
 //! All fields are made public for the purposes of simplicity.
-use std::{collections::HashMap, sync::Arc};
 
-use blake3::{Hash, Hasher};
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
-use tokio::sync::{Mutex, RwLock};
+mod filesystem;
 
-const AVAILABLE_DATES: [NaiveDate; 30] = [
-    NaiveDate::from_ymd_opt(2024, 12, 1).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 2).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 3).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 4).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 5).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 6).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 7).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 8).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 9).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 10).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 11).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 12).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 13).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 14).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 15).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 16).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 17).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 18).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 19).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 20).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 21).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 22).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 23).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 24).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 25).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 26).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 27).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 28).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 29).unwrap(),
-    NaiveDate::from_ymd_opt(2024, 12, 30).unwrap(),
-];
+use arrayvec::ArrayVec;
+use chrono::NaiveDate;
+use futures::{channel::mpsc, StreamExt};
+use std::time::Duration;
 
-const AVAILABLE_TIMES: [NaiveTime; 6] = [
-    NaiveTime::from_hms_opt(0, 0, 0).unwrap(),
-    NaiveTime::from_hms_opt(4, 0, 0).unwrap(),
-    NaiveTime::from_hms_opt(8, 0, 0).unwrap(),
-    NaiveTime::from_hms_opt(12, 0, 0).unwrap(),
-    NaiveTime::from_hms_opt(16, 0, 0).unwrap(),
-    NaiveTime::from_hms_opt(20, 0, 0).unwrap(),
-];
+use libp2p::{
+    gossipsub,
+    mdns,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    PeerId,
+};
+use tracing_subscriber::EnvFilter;
 
-fn gen_date_distribution() -> Vec<NaiveDate> {
-    use rand::seq::SliceRandom;
-    let mut dates: Vec<NaiveDate> = AVAILABLE_DATES
-        .choose_multiple(&mut rand::thread_rng(), 20)
-        .cloned()
-        .collect();
-    dates.sort();
-    dates
+use filesystem::NaiveFs;
+
+// enum NodeRole {
+//     Boot,
+//     Regular,
+// }
+
+/// logic here
+/// we have 3 nodes – [boot, p1, p2]
+///
+/// the sync process for one day is as follows:
+/// - bootnode announces sync day to peers
+/// - p1 & p2 make requests to the bootnode to determine the differences of state between boot<->p1
+/// and boot<->p2
+/// - bootnode exchanges the data with p1 and p2 (bidirectionally) so that their state is equal
+/// - when all network participants signal sync ok for the day, bootnode starts the process for the
+/// next day, continuing up to & including the end point.
+
+/// This is a model of a node, very specifically tied to this test task.
+struct Node {
+    // config of sorts - could be abstracted into a separate struct but we don't care
+    pub sync_start_point: NaiveDate,
+    pub sync_end_point: NaiveDate,
+    // bootnode is the reference point, which sends the first broadcast
+    // pub role: NodeRole,
+    // simulating the three friends here – we will wait to discover everyone through mdns
+    pub peers: ArrayVec<PeerId, 2usize>,
+    pub best_date: NaiveDate,
+    // todo: really? that complicated?
+    pub bump_date_tx: mpsc::Sender<()>,
+    pub bump_date_rx: mpsc::Receiver<()>,
+    pub fs: NaiveFs,
 }
 
-fn gen_image_times(date: NaiveDate) -> Vec<NaiveDateTime> {
-    use rand::seq::SliceRandom;
-    let mut times: Vec<NaiveDateTime> = AVAILABLE_TIMES
-        .choose_multiple(&mut rand::thread_rng(), 3)
-        .cloned()
-        .map(|t| NaiveDateTime::new(date, t))
-        .collect();
-    times.sort();
-    times
-}
+impl Node {}
 
-struct Image {
-    /// the image itself, for the purposes of test task is just random bytes
-    pub data: Vec<u8>,
-    /// the actual date & time value we care about for ordering
-    pub created_at: NaiveDateTime,
-}
-
-impl std::fmt::Debug for Image {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "|{}| {:x?}...", self.created_at, &self.data[..5])
-    }
-}
-
-impl Image {
-    fn random(created_at: NaiveDateTime) -> Self {
-        let rand_slice: [u8; 32] = rand::random();
-        Self {
-            created_at,
-            data: rand_slice.to_vec(),
-        }
-    }
-
-    fn hash(&self) -> Hash {
-        #[cfg(debug_assertions)]
-        tracing::trace!(img_name = %self.created_at, "Hashing self");
-
-        Hasher::new().update_rayon(&self.data).finalize()
-    }
-}
-
-#[derive(Debug)]
-struct Directory {
-    /// actual value we care about for the sync
-    date: NaiveDate,
-    /// images inside of the directory, assuming sorted by creation
-    images: Vec<Image>,
-}
-
-/// This struct holds the metadata of the directory.
-/// Primary sync primitive.
-#[derive(Debug, Clone, Copy)]
-struct DirectoryMetadata {
-    dir_hash: Hash,
-    fs_index: usize,
-}
-
-impl Directory {
-    fn hash(&self) -> Hash {
-        let mut hasher = Hasher::new();
-        for image in self.images.iter() {
-            hasher.update_rayon(&image.data);
-        }
-
-        hasher.finalize()
-    }
-}
-
-#[derive(Debug)]
-struct NaiveFs {
-    /// Root hash of the filesystem. Once all the root hashes
-    /// of the peers become equal, we know the sync is finished.
-    root_hash: Arc<RwLock<Hash>>,
-    /// Assuming that we only have directories in the fs
-    /// The obv choice for such task is an MPT, but i want to rely
-    /// on libraries as least as possible.
-    ///
-    /// the RwLock is used for easy concurrent accesses
-    dir_metadatas: Arc<RwLock<HashMap<NaiveDate, DirectoryMetadata>>>,
-    dirs: Arc<Mutex<Vec<Directory>>>,
-}
-
-impl NaiveFs {
-    fn random() -> Self {
-        let available_dates = gen_date_distribution();
-        let mut root_hasher = Hasher::new();
-        let dirs: Vec<(NaiveDate, Directory, DirectoryMetadata)> = available_dates
-            .into_iter()
-            .enumerate()
-            .map(|(idx, d)| {
-                let dir = Directory {
-                    date: d,
-                    images: gen_image_times(d)
-                        .into_iter()
-                        .map(|t| Image::random(t))
-                        .collect(),
-                };
-                let dir_hash = dir.hash();
-                root_hasher.update_rayon(dir_hash.as_bytes());
-                let dir_metadata = DirectoryMetadata {
-                    dir_hash,
-                    fs_index: idx,
-                };
-                (d, dir, dir_metadata)
-            }) // return both the date and the dir to build
-            // the hashmap
-            .collect();
-        let directory_metadatas = 
-                dirs.iter().map(|(date, _, meta)| (*date, *meta)).collect::<Vec<(NaiveDate, DirectoryMetadata)>>();
-        let dirs = dirs.into_iter().map(|(_, dir, _)| dir).collect();
-
-        Self {
-            root_hash: Arc::new(RwLock::new(root_hasher.finalize())),
-            dir_metadatas: Arc::new(RwLock::new(HashMap::from_iter(
-                directory_metadatas
-            ))),
-            dirs: Arc::new(Mutex::new(dirs)),
-        }
-    }
+// We create a custom network behaviour that combines Gossipsub and Mdns.
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::tokio::Behaviour,
 }
 
 #[tokio::main]
-async fn main() {
-    let fs = NaiveFs::random();
-    dbg!(fs);
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let _fs = filesystem::NaiveFs::random();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::noise::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            // To content-address message, we can take the hash of message and use it as an ID.
+            let message_id_fn = |message: &gossipsub::Message| {
+                let message_id = blake3::hash(&message.data);
+                gossipsub::MessageId(message_id.as_bytes().to_vec())
+            };
+
+            // Set a custom gossipsub configuration
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
+                // signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .build()?;
+
+            // build a gossipsub network behaviour
+            let gossipsub = gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(key.clone()),
+                gossipsub_config,
+            )?;
+
+            let mdns =
+                mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
+            Ok(MyBehaviour { gossipsub, mdns })
+        })?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely.
+        .build();
+
+    // Tell the swarm to listen on localhost and a random, OS-assigned
+    // port.
+    // swarm.listen_on("/ip4/127.0.0.1/udp/0/quic-v1".parse()?)?;
+    swarm.listen_on("/ip4/127.0.0.1/tcp/0".parse()?)?;
+
+    let topic = gossipsub::IdentTopic::new("image-sync");
+    // subscribes to our topic
+    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
+
+    // TODO: for this task we NEED a linear flow:
+    // 1. wait for the required number of peers
+    // 2. kick of communication based on date with all the peers
+    // 2.1. for each peer,
+    loop {
+        tokio::select! {
+            // Ok(Some(line)) = stdin.next_line() => {
+            //     if let Err(e) = swarm
+            //         .behaviour_mut().gossipsub
+            //         .publish(topic.clone(), line.as_bytes()) {
+            //         println!("Publish error: {e:?}");
+            //     }
+            // }
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                        // println!("current peers: {:?}", swarm.behaviour().gossipsub.all_peers().collect::<Vec<(&PeerId, Vec<&TopicHash>)>>());
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => println!(
+                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                        String::from_utf8_lossy(&message.data),
+                    ),
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Local node is listening on {address}");
+                }
+                _ => {}
+            }
+        }
+    }
 }
